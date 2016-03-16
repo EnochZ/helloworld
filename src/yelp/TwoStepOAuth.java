a39bb9d8051d65868b526741075c4874bcbaeeed
angelaHang <angelahang186@gmail.com>
2016-03-15 21:24:52 -0700

    First commit

---
+++ src/yelp/TwoStepOAuth.java
@@ -0,0 +1,18 @@
+package yelp;
+import org.scribe.builder.api.DefaultApi10a;
+import org.scribe.model.Token;
+
+public class TwoStepOAuth extends DefaultApi10a {
+	public String getAccessTokenEndpoint() {
+		return null;
+	  }
+
+	  public String getAuthorizationUrl(Token arg0) {
+		return null;
+	  }
+
+	  public String getRequestTokenEndpoint() {
+		return null;
+	  }
+
+}
